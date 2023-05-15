61d5c6597d0a62e5fdea922d03eeb712fa7e52fc
dimzles <dimzlestv@gmail.com>
2023-05-15 17:29:52 +0100

    Add buildTree function

--- bst.js
+++ bst.js
@@ -5,3 +5,21 @@ class Node {
     this.rightNode = null;
   }
 }
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+
+    const mid = parseInt((start + end) / 2);
+    const node = new Node(array[mid]);
+
+    node.leftNode = this.buildTree(array, start, mid - 1);
+    node.rightNode = this.buildTree(array, mid + 1, end);
+
+    return node;
+  }
+}
